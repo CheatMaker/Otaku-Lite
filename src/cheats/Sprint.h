f378198132e34a4d87017680f0abd6714626c1eb
DiamondCoder1000 <78707555+DiamondCoder1000@users.noreply.github.com>
2022-11-23 20:12:06 -0800

    Update Sprint.h

--- src/cheats/Sprint.h
+++ src/cheats/Sprint.h
@@ -1,1 +1,19 @@
+//
+// Created by DiamondCoder1000 on 11/23/22
+//
 
+#ifndef PHANTOM_SPRINT_H
+#define PHANTOM_SPRINT_H
+
+#include "Cheat.h"
+
+class EntityPlayer;
+class AimAssist : public Cheat {
+public:
+    explicit Sprint(Phantom *phantom);
+
+    void run(Minecraft *mc) override;
+};
+
+
+#endif //PHANTOM_SPRINT_H
