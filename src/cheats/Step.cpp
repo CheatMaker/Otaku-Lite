c07fe8d6a73c41c45cf0f51f87a1d87f7cd0edec
DiamondCoder1000 <78707555+DiamondCoder1000@users.noreply.github.com>
2022-11-24 11:35:14 -0800

    Create Step.cpp

---
+++ src/cheats/Step.cpp
@@ -0,0 +1,18 @@
+#include "Step.h"
+
+Step::Step (Minecraft *mc) {
+    stepHeight = 1;
+}
+
+void Step::Run(GameMode* GM) {
+	*player->stepHeight() = stepHeight;
+}
+
+void Step::onDisable() {
+	if (player == nullptr) return;
+	*player->stepHeight() = 0.5625f;
+}
+
+void Step::renderSettings() {
+  ImGui::SliderFloat("step height", &stepHeight, 0.5625, 3);
+}
