cc65a119747fc153e1bfe954945745a9cb439410
DiamondCoder1000 <78707555+DiamondCoder1000@users.noreply.github.com>
2022-11-23 21:01:33 -0800

    Update Speed.h

--- src/cheats/Speed.h
+++ src/cheats/Speed.h
@@ -1,1 +1,19 @@
+#ifndef PHANTOM_SPEED_H
+#define PHANTOM_Speed_H
 
+
+#include "Cheat.h"
+
+class PlayerCapabilities;
+class Speed : public Cheat {
+public:
+    explicit Speed(Phantom *phantom);
+
+    void run(Minecraft *mc) override;
+    void renderSettings() override;
+private:
+    Phantom *phantom;
+};
+
+
+#endif //PHANTOM_SPEED_H
