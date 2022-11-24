6d8d2a7b99625e39a0a4d813abb6fad86f316cac
DiamondCoder1000 <78707555+DiamondCoder1000@users.noreply.github.com>
2022-11-23 20:54:01 -0800

    Update Speed.cpp

--- src/cheats/Speed.cpp
+++ src/cheats/Speed.cpp
@@ -1,1 +1,15 @@
+#include "net/minecraft/entity/player/PlayerCapabilities.h"
+Speed::Speed(Phantom *phantom) : Cheat("AimAssist", "Aims for u, but smoooothly.") {
+    this->phantom = phantom;
 
+    float float_t69 = 1;
+    
+}
+
+void Speed::run(Minecraft *mc) {
+    PlayerCapabilities.setPlayerWalkingSpeed(float_t69);
+}
+
+void Speed::renderSettings() {
+    ImGui::SliderFloat("Speed", &float_t69, 0, 6, "%.2f");   
+}
