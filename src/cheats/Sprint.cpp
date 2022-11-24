c3cbabbb3bdb6e4e40b32c24373297619b823ab6
DiamondCoder1000 <78707555+DiamondCoder1000@users.noreply.github.com>
2022-11-23 20:43:16 -0800

    Create Sprint.cpp

---
+++ src/cheats/Sprint.cpp
@@ -0,0 +1,12 @@
+//
+// Created by DiamondCoder1000 on 11/23/22.
+//
+
+#include "Sprint.h"
+
+Sprint::Sprint() : Cheat("Sprint", "Like ToggleSprint") {}
+
+void Sprint::run(Minecraft *mc) {
+    //This does NOT work 
+    mc->IsSprinting(True);
+}
